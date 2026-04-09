07ee37f1ec5605ff6b1d936bea39537cfdb6431d
ridaallahrochdi <ridaallahrochdi@gmail.com>
2026-04-09 14:51:57 +0100

    feat: add fillable fields and relationships to Candidat model and define api routes

--- database/migrations/2026_04_05_124508_create_candidats_table.php
+++ database/migrations/2026_04_05_124508_create_candidats_table.php
@@ -11,14 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('candidats', function (Blueprint $table) {
+       Schema::create('candidats', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('prenom');
+            $table->date('dateNaissance')->nullable();
             $table->string('telephone');
+            $table->string('email');                  
+            $table->string('cv')->nullable();          
+            $table->string('photo')->nullable();       
+            $table->text('experience')->nullable();    
+            
+            // (Foreign Keys)
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('ville_id')->constrained('villes'); 
+            
             $table->timestamps();
-        });
+});
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OffreController;
 use App\Http\Controllers\CandidatureController;
+use App\Http\Controllers\CandidatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,16 @@ Route::middleware('auth:sanctum')->group(function () {
     // [JNV-22] Apply for job (candidat only)
     Route::post('/candidatures', [CandidatureController::class, 'store'])
         ->middleware('check.role:candidat');
+});
+
+
+Route::middleware(['auth:sanctum', 'check.role:candidat'])->group(function () {
+    
+    // JNV-7 and JNV-8: CV
+    
+    Route::post('/candidat/profile', [CandidatController::class, 'updateProfile']); 
+    
+    // JNV-23:  (Candidatures)
+    Route::get('/candidat/candidatures', [CandidatController::class, 'indexCandidatures']);
+
 });
